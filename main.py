a0b833ddca98dbdf949ea45926db2eb00d933c7d
ejarao <irishegie.arao@ssu.edu.ph>
2024-09-26 08:44:05 +0800

    first: initializing repo

---
+++ main.py
@@ -0,0 +1,7 @@
+import requests as rq
+import os
+
+
+# API_KEY = 
+#This file will need to use the DataManager,FlightSearch, FlightData, NotificationManager classes to achieve the program requirements.
+
